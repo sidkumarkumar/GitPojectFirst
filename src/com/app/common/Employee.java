bb3ec9dfe2a3beefe559a8fedff552df3de63efd
sarved <sarved@EV>
2019-05-09 14:51:47 +0530

    Modifyed

---
+++ src/com/app/common/Employee.java
@@ -0,0 +1,22 @@
+package com.app.common;
+
+public class Employee {
+	private int empId;
+	private String empSal;
+	public Employee() {
+		super();
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpSal() {
+		return empSal;
+	}
+	public void setEmpSal(String empSal) {
+		this.empSal = empSal;
+	}
+
+}
